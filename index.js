79294baa112af6c9d75bbd5e0008245fa0c46b53
Brian Juma <bjuma0985@gmail.com>
2024-03-22 13:47:03 +0300

    continuing function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return "This Saturday, I want to " + activity + "!";
+    console.log(result);
+}
+function mondayWork(task = "go to the office"){
+    return "This Monday, I will " + task + ".";
+    console.log(result);
+}
+function wrapAdjective(hardworker = "*"){
+    return function(adjective = "special"){
+        return "You are " + hardworker + adjective + hardworker + "!";
+    }
+
+}
